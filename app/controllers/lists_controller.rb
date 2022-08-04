6a2b127dbae11781ec915c4b0485a5e2252f14d9
takotakosansan <tako.noboriya@icloud.com>
2022-08-04 13:01:56 +0000

    詳細画面まで

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,32 @@
+class ListsController < ApplicationController
+  def new
+     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new  
+  end
+  
+  def create
+    list = List.new(list_params)
+    list.save
+    # redirect_to '/top' を削除して、以下コードに変更
+    # 詳細画面へリダイレクト
+    redirect_to list_path(list.id)  
+  end
+
+  def index
+     @lists = List.all  
+  end
+
+  def show
+     @list = List.find(params[:id])  
+  end
+
+  def edit
+  end
+  
+   private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'lists/new'
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
+   get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/edit'
   devise_for :users
   root to: 'homes#top'
   get "homes/about" => "homes#about" 
