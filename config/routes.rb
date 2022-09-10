5414af2e00bfe7ccc64871e8440a9ef69810ad4a
takotakosansan <tako.noboriya@icloud.com>
2022-09-10 06:43:10 +0000

    favorite

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "homes/about" => "homes#about" 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
   
+    resources :post_comments, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
